ddcc5e1c216683f9e7d49b5d3bfa3ddb685bb505
Robson Santos <robson.eng@hotmail.com>
2020-04-21 10:15:34 -0300

    Primeiro deploy

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,9 +15,10 @@ const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
     <header>
       <img src={Logo} alt="GoFinances" />
       <nav>
-        {
-          // Todo
-        }
+        <div>
+          <Link to="/">Listagem</Link>
+          <Link to="/import">Importar</Link>
+        </div>
       </nav>
     </header>
   </Container>
--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,31 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
     // TODO
+    uploadedFiles.forEach((uploadedFile) =>
+      data.append('file', uploadedFile.file),
+    );
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err);
     }
   }
 
   function submitFile(files: File[]): void {
     // TODO
+    const filesToUpload: FileProps[] = [];
+    files.forEach((file) => {
+      filesToUpload.push({
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      });
+    });
+    setUploadedFiles(uploadedFiles.concat(filesToUpload));
   }
 
   return (
